b3638a44f94033c8d6e32f748fec2d23cea35d3b
MD. RAKIB HASAN <hasanrakibcse@gmail.com>
2024-03-07 12:44:31 +0600

    show appointments done

--- index.js
+++ index.js
@@ -36,10 +36,11 @@ async function run() {
 
     const db = await client.db(`${process.env.DB_NAME}`);
     const appointmentCollection = await db.collection(`${process.env.APPOINTMENT_COLL_NAME}`);
+    const doctorCollection = await db.collection(`${process.env.DOCTORS_COLL_NAME}`);
 
     // View root directory
     app.get('/', async (req, res) => {
-      res.send("Hello Ema-John Server. It's Working");
+      res.send("Hello Ema-John Server. It's Working.....");
     })
 
     // CREATE OPERATION
@@ -53,7 +54,83 @@ async function run() {
           res.send(result.acknowledged);
           console.log(result.acknowledged)
         })
+    });
+
+    // See All Appointments
+    app.get('/appointments', async (req, res) => {
+      await client.connect();
+
+      const cursor = appointmentCollection.find({})
+      // const service = await cursor.toArray((err, document) => {
+      //   res.send(document);
+      // })
+      const services = await cursor.toArray()
+      res.send(services);
     })
+
+    // FIND OPERATION
+    app.post('/appointmentsByDate', async (req, res) => {
+      await client.connect();
+
+      const date = req.body;
+      console.log(date.date)
+
+      const cursor = appointmentCollection.find({ date: date.date })
+      // const services = await cursor.toArray((err, document) => {
+      //   res.send(document);
+      // })
+      const services = await cursor.toArray()
+      res.send(services);
+    });
+
+    // Add Doctor
+    app.post('/addADoctor', async (req, res) => {
+      const file = req.files.file;
+      const name = req.body.name;
+      const email = req.body.email;
+      const newImg = file.data;
+      const encImg = newImg.toString('base64');
+
+      var image = {
+        contentType: file.mimetype,
+        size: file.size,
+        img: Buffer.from(encImg, 'base64')
+      };
+
+      await doctorCollection.insertOne({ name, email, image })
+        .then((result) => {
+          res.send(result.acknowledged);
+          console.log(result.acknowledged)
+        })
+    })
+
+    // All Doctors
+    app.get('/doctors', async (req, res) => {
+      const cursor = doctorCollection.find({})
+      const service = await cursor.toArray((err, document) => {
+        res.send(document);
+      })
+    });
+
+    // Is Doctor
+    app.post('/isDoctor', async (req, res) => {
+      const email = await req.body.email;
+
+      const cursor = doctorCollection.find({ email: email })
+      const service = await cursor.toArray((err, doctors) => {
+        res.send(doctors.acknowledged);
+        console.log(doctors.acknowledged)
+      })
+    })
+
+    // app.post('/isDoctor', (req, res) => {
+    //   const email = req.body.email;
+    //   doctorCollection.find({ email: email })
+    //     .toArray((err, doctors) => {
+    //       res.send(doctors.length > 0);
+    //     })
+    // })
+
 
   } finally {
     await client.close();
